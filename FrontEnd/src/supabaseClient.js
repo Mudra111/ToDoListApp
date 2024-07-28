cbe7336a09c91b3480204b7fb177176422d27c3d
Mudra Bhalodiya <mudrabhalodiya@gmail.com>
2024-07-28 17:29:57 +0530

    environment variables edited

--- FrontEnd/src/supabaseClient.js
+++ FrontEnd/src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://emzdovcukfjmrteynkci.supabase.co";
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtemRvdmN1a2ZqbXJ0ZXlua2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwMDUxMzQsImV4cCI6MjAzNzU4MTEzNH0.4166SxDy6BYLDkAIhCB3PwobzhmSpTLtpCrn-0W8DWI";
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseKey = process.env.SUPABASEKEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
